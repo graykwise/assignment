1a3c07a834d3d6208b4992fa7b81691549161a29
Grayson Wise <graysonwise3@gmail.com>
2017-05-21 12:44:46 -0400

    made iOS project

---
+++ first_assignment/first_assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first_assignment
+//
+//  Created by Grayson Wise on 5/21/17.
+//  Copyright © 2017 Grayson Wise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
